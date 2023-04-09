fe65e89346d30c906258a33128c20f65ec267add
AmonKipkoech <amonkiprop68@gmail.com>
2023-04-09 13:44:35 +0300

    messasge

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
- if (process.env.NODE_ENV !== 'production'){
-      require('dotenv').load()  
-
- }
- 
- const stripeSecretKey = process.env.STRIPE_SECRET_KEY
- const stripePublicKey = process.env.STRIPE_Public_KEY
-      console.log(stripeSecretKey, stripePublicKey)
- 
 const express = require('express')
 const app = express()
+const fs = require('fs')
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
+
+app.get('/store', function( req, res){
+     fs.readFile('item.json', function(error, data){
+          if(error){
+               res.status(500).end()
+          } 
+          else{
+                    res.render('store.ejs',{
+                         Items: JSON.parse(data)
+                    })
+          }
+     })
+})
 
 app.listen(3000)    
